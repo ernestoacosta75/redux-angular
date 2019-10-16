92fb0fc5db4e99baf77d12a667f9bc914f2f11ba
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-10-16 22:09:24 +0200

    Configuring more actions for the reducer

--- app-1.ts
+++ app-1.ts
@@ -1,5 +1,6 @@
 /**
  * My first action.
+ * An action is just an interface.
  */
 interface Action {
     type: string;
@@ -9,18 +10,56 @@ interface Action {
 /**
  * My first reducer.
  * The reducer always must return an state.
+ * Basically, it's a switch with all the actions
+ * that it can operate.
  */
 function reducer( state = 10, action: Action ) {
-    if (action.type === 'INCREMENT') {
-        return state += 1;
+    switch( action.type ) {
+        case 'INCREMENT':
+            return state +=1;
+            break;
+
+        case 'DECREMENT':
+            return state -=1;
+            break;   
+            
+        case 'MULTIPLY':
+            return state * action.payload;
+            break;
+              
+        case 'DIVIDE':
+            return action.payload !== 0 ? state / action.payload : state; 
+            break;
+
+        default:
+            return state;
+            break;    
     }
-    
-    return state;
 }
 
 // Using the reducer
-const incrementatorAction: Action = {
+const incrementAction: Action = {
     type: 'INCREMENT'
 }
 
-console.log(reducer(10, incrementatorAction));
+const decrementAction: Action = {
+    type: 'DECREMENT'
+}
+
+const multiplyAction: Action = {
+    type: 'MULTIPLY',
+    payload: 2 
+}
+
+const divideAction: Action = {
+    type: 'DIVIDE',
+    payload: 2 
+}
+
+console.log(reducer(10, incrementAction));
+
+console.log(reducer(10, decrementAction));
+
+console.log(reducer(10, multiplyAction));
+
+console.log(reducer(10, divideAction));
