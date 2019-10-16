dcfa27b7cbabe612aaef44cf3f392c93d78b2ac3
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-10-16 21:49:13 +0200

    Basic action and reducer

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,26 @@
-console.log('Hello world');
+/**
+ * My first action.
+ */
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+/**
+ * My first reducer.
+ * The reducer always must return an state.
+ */
+function reducer( state = 10, action: Action ) {
+    if (action.type === 'INCREMENT') {
+        return state += 1;
+    }
+    
+    return state;
+}
+
+// Using the reducer
+const incrementatorAction: Action = {
+    type: 'INCREMENT'
+}
+
+console.log(reducer(10, incrementatorAction));
